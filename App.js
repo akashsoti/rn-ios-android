1080e1fc916d9afa22ab837219236f1d7076c8cb
Akash Soti <akashsoti@gmail.com>
2019-05-29 09:46:54 +0530

    Make the input work

--- App.js
+++ App.js
@@ -1,53 +1,27 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, { Component } from "react";
+import { StyleSheet, View } from "react-native";
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, TextInput, Button} from 'react-native';
-import ListItem from "./src/components/ListItem/ListItem";
-import TextInputss from "./src/components/TextInputss/TextInputss";
+import PlaceInput from "./src/components/PlaceInput/PlaceInput";
+import PlaceList from "./src/components/PlaceList/PlaceList";
 
-export default class App extends Component<Props> {
-
+export default class App extends Component {
   state = {
-    placeName: '',
     places: []
   };
 
-  placeNameChangeHandler = val => {
-    this.setState({
-      placeName: val
-    });
-  };
-
-  placeNameSubmitHandler = () => {
-    if (this.state.placeName.trim() === ""){
-      return;
-    }
+  placeAddedHandler = placeName => {
     this.setState(prevState => {
       return {
-        places: prevState.places.concat(prevState.placeName)
+        places: prevState.places.concat(placeName)
       };
     });
   };
 
   render() {
-    const placesOutput = this.state.places.map((place,i) => (
-      <ListItem key={i} placeName={place} />
-    ));
     return (
       <View style={styles.container}>
-        <View style={styles.textInputContainer}>
-          <TextInputss value={this.state.placeName} placeholder="Hello" onChangeText={this.placeNameChangeHandler}/>
-        </View>
-        <Button title="Add" onPress={this.placeNameSubmitHandler}/>
-        <View style={styles.listContainer}>
-          {placesOutput}
-        </View>
+        <PlaceInput onPlaceAdded={this.placeAddedHandler} />
+        <PlaceList places={this.state.places} />
       </View>
     );
   }
@@ -55,25 +29,10 @@ export default class App extends Component<Props> {
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: 60,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  listContainer: {
-    width: "100%"
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-  textInputContainer: {
-    width: "60%"
+    flex: 1,
+    padding: 46,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "flex-start"
   }
 });
