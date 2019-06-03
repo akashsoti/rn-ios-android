2cc1dab47032a5acdc34bcf2c0fc10c4d62a21c7
Akash <akashsoti@gmail.com>
2019-06-03 15:06:44 +0530

    use flatlist instead of scrollview

--- src/components/PlaceList/PlaceList.js
+++ src/components/PlaceList/PlaceList.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { StyleSheet, FlatList } from 'react-native';
 
 import ListItem from '../ListItem/ListItem';
 
 const placeList = props => {
-	const placesOutput = props.places.map((place, i) => (
-		<ListItem key={i} placeName={place} onItemPressed={()=> props.onItemDeleted(i)}/>
-	));
 	return (
-		<View style={styles.listContainer}>{placesOutput}</View>
+		<FlatList data={props.places} renderItem={(info) => (<ListItem placeName={info.item.value} onItemPressed={()=> props.onItemDeleted(info.item.key)}/>)} style={styles.listContainer}></FlatList>
 	);
 };
 
