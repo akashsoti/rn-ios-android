b833f8c3050fabaa07d12f8bafb7ac461a8470a9
Akash <akashsoti@gmail.com>
2019-06-03 13:46:57 +0530

    Add touchable class

--- src/components/PlaceInput/PlaceInput.js
+++ src/components/PlaceInput/PlaceInput.js
@@ -4,30 +4,31 @@ import { View, TextInput, Button, StyleSheet } from "react-native";
 class PlaceInput extends Component {
   state = {
     placeName: ""
-  };
+  }; //initialising the state for the input text field which is empty by default
 
   placeNameChangedHandler = val => {
     this.setState({
       placeName: val
-    });
-  };
+    }); //captures the value that is being filled in the text input and sets the state of the textinput to the value that is being passed
+  }; 
 
   placeSubmitHandler = () => {
     if (this.state.placeName.trim() === "") {
       return;
-    }
+    } //it checks if the textinput value is empty. If yes then it just returns it and not reder
 
-    this.props.onPlaceAdded(this.state.placeName);
+    this.props.onPlaceAdded(this.state.placeName); //else there is a prop called onPlaceAdded which stores the name entered in the input field in the state of placeName
   };
+  // is onPlaceAdded a prop of PlaceInput? is yes than it is inside a function and in a if else loop
 
   render() {
     return (
       <View style={styles.inputContainer}>
         <TextInput
-          placeholder="Enter a name"
-          value={this.state.placeName}
-          onChangeText={this.placeNameChangedHandler}
-          style={styles.placeInput}
+          placeholder="Enter a name" // set a placeholder
+          value={this.state.placeName} //sets empty value to this textinput
+          onChangeText={this.placeNameChangedHandler} //onchange is a method provided by react-native which is call placenamechamgehandler, which is setting the value entered as the state
+          style={styles.placeInput} //you have to set everything
         />
         <Button
           title="Add"
